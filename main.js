e07d88bc6be549fb2d186218ecc0dbabd8af91fe
NahidaSultana <freelancernahidasultana@gmail.com>
2023-09-24 15:58:21 +0600

    java problem solving -1

---
+++ main.js
@@ -0,0 +1,28 @@
+// problem solving-1
+// find the maximum between two numbers.
+
+let num1 = 10;
+let num2 = 20;
+
+if (num1 > num2){
+console.log(num1,"is greter");
+
+}else{
+    console.log(num2,"is greter");
+}
+
+// problem solving-2
+// find a maximum between three numbers.
+
+let numa = 30;
+let numb = 40;
+let numc = 50;
+
+if (numa > numb){
+console.log(num1,"is greter");
+
+}else{
+    console.log(num2,"is greter");
+}
+
+if(numb>)
